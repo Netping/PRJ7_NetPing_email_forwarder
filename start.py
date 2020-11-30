8895e5188ddbde9316076bd717763235e8441ed4
igor87z <igor87z@gmail.com>
2020-11-30 23:31:24 +0300

    Clean code

--- start.py
+++ start.py
@@ -1,15 +1,17 @@
 from db import create_db
 from config import VERSION, get_parser, get_loggers
 
+
 def run(config):
-    log, error_log = get_loggers(config.log_dir, config.error_log_dir, 'start.py')
+    log, error_log = get_loggers(config.log_dir, config.error_log_dir,
+                                 'start.py')
     log.info(f'Start NetPIng Email forwarder v.{VERSION}.')
-    
+
     admin_db = create_db(config.admin_db_host, config.admin_db_port,
                          config.admin_db_username, config.admin_db_password,
                          config.admin_db_name)
     admin_db.check_structure('admin')
-    
+
     user_db = create_db(config.user_db_host, config.user_db_port,
                         config.user_db_username, config.user_db_password,
                         config.user_db_name)
