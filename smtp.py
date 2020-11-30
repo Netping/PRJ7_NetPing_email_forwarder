1ae76204c54944db5b6508b21f5caba2e530a95e
igor87z <igor87z@gmail.com>
2020-11-30 22:08:36 +0300

    Start SMTP server

--- smtp.py
+++ smtp.py
@@ -1,12 +1,32 @@
+import asyncore
+
 from config import VERSION, get_parser, get_loggers
+from db import create_db
+from mails.mailbox import MailBox
+from smtp_server.smtp_server import ProxySMTPServer
+
+
+def run_smtp_proxy(config):
+    ProxySMTPServer((config.smtp_host, config.smtp_port),
+                    (config.smtp_login, config.smtp_pass),
+                    mailbox, log, error_log)
+    asyncore.loop()
 
 
 if __name__ == '__main__':
-    config = get_parser(f'NetPIng Email forwarder v.{VERSION}, SMTP-Proxy server.',
-                        ('user_db', 'smtp')).parse_args()
-    log, error_log = get_loggers(config.log_dir, config.error_log_dir, 'smtp.py')
+    config = get_parser(
+        f'NetPIng Email forwarder v.{VERSION}, SMTP-Proxy server.',
+        ('user_db', 'smtp')).parse_args()
+    log, error_log = get_loggers(config.log_dir, config.error_log_dir,
+                                 'smtp.py')
     log.info(f'NetPIng Email forwarder v.{VERSION}, SMTP-Proxy server.')
 
     user_db = create_db(config.user_db_host, config.user_db_port,
                         config.user_db_username, config.user_db_password,
                         config.user_db_name)
+
+    mailbox = MailBox(user_db, log, error_log)
+
+    ProxySMTPServer((config.smtp_host, config.smtp_port),
+                    (config.smtp_login, config.smtp_pass),
+                    mailbox, log, error_log)
